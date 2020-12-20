542653bc0a0a9425198fa0acc978bff6e00b8b45
LILSchmidty <gerhartdschmidt2233@gmail.com>
2020-12-20 12:37:42 +0000

    New: can add events, can add bdays, remove bdays and event. show all + show today

--- main.py
+++ main.py
@@ -1,130 +1,276 @@
 import discord
-import os 
+import os
 import time
 from datetime import datetime
 from replit import db
+import sqlite3
 
 client = discord.Client()
 
+conn = sqlite3.connect('test.db')
+print("Opened database successfully")
+cursor = conn.cursor()
 
-def check_DB():
-  return 0
+#Create tables
+conn.execute('''CREATE TABLE IF NOT EXISTS Events
+         (ID INTEGER PRIMARY KEY     AUTOINCREMENT,
+         NAME           TEXT     NOT NULL,
+         Date           TEXT     NOT NULL,
+         Reminder       TEXT     NOT NULL);''')
+print("Event Table created successfully")
 
+conn.execute('''CREATE TABLE IF NOT EXISTS Bday_table
+         (ID INTEGER PRIMARY KEY NOT NULL,
+         Date           TEXT     NOT NULL,
+         Reminder       TEXT     );''')
+print("Bday Table created successfully")
+
+conn.close()
+
+
+#formats user id to @ them
 def at_user(id):
-  out = "<@"+id+">"
-  return out
+    out = "<@"+id+">"
+    return out
 
-
-def get_event_id():
-  keys = db.keys()
-  out = len(keys)
-  return out
-
-
-def add_event(id, event_name, event_date_obj, event_remider_obj):
-  if id in db:
-    print("Invalid")
-  else:
-    db[id] = [event_name, event_date_obj, event_remider_obj]
-
-def delete_event(id):
-  out = ""
-  keys = db.keys()
-  if len(keys) > id: 
-    out = db[id] 
-    del db[id]
-  return out
-
+#add event
+def add_event(event_name, event_date, event_remider):
   
+  conn = sqlite3.connect('test.db')
+  cursor = conn.cursor()
+  
+  conn.execute("INSERT INTO Events (NAME, Date, Reminder) VALUES(?, ?, ?)", 
+                                                  (event_name, event_date, event_remider)) 
+  conn.commit()
+  cursor.close()
+
+#add bday
+def add_bday(id, bday_date, bday_remider):
+  
+  conn = sqlite3.connect('test.db')
+  cursor = conn.cursor()
+  
+  conn.execute("INSERT INTO Bday_table (id, Date, Reminder) VALUES(?, ?, ?)", 
+                                                  (id, bday_date, bday_remider)) 
+  conn.commit()
+  cursor.close()
+
+#show all events
+def show_all():
+  out = "Events: \n"
+  names = []
+  dates = []
+  ids = []
+  #SQL
+  conn = sqlite3.connect('test.db')
+  cursor = conn.cursor()
+  cursor.execute('''Select Name from Events;''') 
+  names = cursor.fetchall();
+
+  cursor.execute('''Select Date from Events;''') 
+  dates = cursor.fetchall(); 
+
+  conn.commit()
+  cursor.close()
+
+  conn = sqlite3.connect('test.db')
+  cursor = conn.cursor()
+
+  for i in range(len(names)):
+    str_name = names[i][0]
+    str_date = dates[i][0]
+    out = out + str_name + ": " + str_date + "\n"
+  
+  out = out + "\n" + "Birthdays: \n"
+
+  cursor.execute('''Select ID from Bday_table;''') 
+  ids = cursor.fetchall();
+
+  cursor.execute('''Select Date from Bday_table;''') 
+  dates = cursor.fetchall(); 
+  
+  conn.commit()
+  cursor.close()
+
+  for i in range(len(ids)):
+    member = client.get_user(int(ids[i][0]))
+    str_name = member.name
+    str_date = dates[i][0]
+    out = out + str_name + ": " + str_date + "\n"
+  return out
+  
+#delete event given event Name
+def delete_event(event_name):
+  sql = 'DELETE FROM Events WHERE name=?'
+  conn = sqlite3.connect('test.db')
+  cursor = conn.cursor()
+  cursor.execute(sql, (event_name,))
+  conn.commit()
+
+#deletBday given userid
+def delete_bday(id):
+  sql = 'DELETE FROM Bday_table WHERE id=?'
+  conn = sqlite3.connect('test.db')
+  cursor = conn.cursor()
+  cursor.execute(sql, (id,))
+  conn.commit()
+  out = "Deleted Birthday"
+  return out
+
+#Delete all Replit DB
+def delete_all_Replit_DB():
+  all_keys = db.keys()
+  for key in all_keys:
+    del db[key]  
+  print("All deleted")
+
+#Delete all events
+def delete_all():
+  out = "All events deleted!"
+  
+  conn = sqlite3.connect('test.db')
+  cursor = conn.cursor()
+  cursor.execute('''Delete from Events;''')  
+
+  conn.commit()
+  cursor.close()
+
+  return out
+
+def today():
+  current_date = datetime.today().date()
+  out = "Things happening today \n \nEvents: \n"
+  names = []
+  dates = []
+  ids = []
+  #SQL
+  conn = sqlite3.connect('test.db')
+  cursor = conn.cursor()
+  cursor.execute('''Select Name from Events;''') 
+  names = cursor.fetchall();
+
+  cursor.execute('''Select Date from Events;''') 
+  dates = cursor.fetchall(); 
+
+  conn.commit()
+  cursor.close()
+
+  conn = sqlite3.connect('test.db')
+  cursor = conn.cursor()
+
+  for i in range(len(names)):
+    str_name = names[i][0]
+    str_date = dates[i][0]
+    date_time_obj = datetime.strptime(str_date, '%Y-%m-%d %H:%M:%S')
+    i_date = date_time_obj.date()
+    if i_date == current_date:
+      out = out + str_name + "\n"
+  
+  out = out + "\n" + "Birthdays: \n"
+
+  cursor.execute('''Select ID from Bday_table;''') 
+  ids = cursor.fetchall();
+
+  cursor.execute('''Select Date from Bday_table;''') 
+  dates = cursor.fetchall(); 
+  
+  conn.commit()
+  cursor.close()
+
+  for i in range(len(ids)):
+    member = client.get_user(int(ids[i][0]))
+    str_name = member.name
+    str_date = dates[i][0]
+    date_time_obj = datetime.strptime(str_date, '%d/%m/%Y')
+    
+    i_date = date_time_obj.date()
+
+    if (int(i_date.day) == int(current_date.day)) and (int(i_date.month) == int(current_date.month)):
+      out = out + str_name + "\n"
+
+  return out
+
 #boot up
 @client.event
 async def on_ready():
-  print('We have logged on as {0.user}'.format(client))
-  all_keys = db.keys()
-  print(all_keys)
+    print('We have logged on as {0.user}'.format(client))
 
 #responses
 @client.event
 async def on_message(message):
-  #bot message
-  if message.author == client.user:
-    return
-  
-  msg = message.content
-  
-  #hello
-  if msg.startswith('$Hello'):
-    await message.channel.send("Wazzup")
-  
-  #Time
-  if msg.startswith('$Time?'):
-    current_time = time.asctime( time.localtime(time.time()))
-    await message.channel.send(current_time)
-
-  #Set event 
-  if msg.startswith('$Event'):
     
-    event_arr = msg.split(",");
+    #bot message
+    if message.author == client.user:
+        return
+
+    msg = message.content
+
+    #hello
+    if msg.startswith('$Hello'):
+        await message.channel.send("Wazzup")
+
+    #Get current date and time
+    if msg.startswith('$Time?'):
+        current_time = time.asctime( time.localtime(time.time()))
+        await message.channel.send(current_time)
+
+    #Add event command
+    if msg.startswith('$Event'):
+
+        event_arr = msg.split(", ");
+
+        #get event parameters
+        event_name = event_arr[1]
+        event_date =  event_arr[2]
+        event_remider =  event_arr[3]
+        event_date_obj = datetime.strptime(event_date, '%d/%m/%Y %H:%M:%S')
+        event_remider_obj = datetime.strptime(event_remider, '%d/%m/%Y %H:%M:%S')
+        add_event(event_name, event_date_obj, event_remider_obj)
+
+        await message.channel.send("Your event named: " + event_name + " that will occur on " + event_date + " has been added.")
+
+    #Add Bday command
+    if msg.startswith('$Bday'):
+        event_arr = msg.split(",");
+        bday_id = event_arr[1][4:-1]
+        member = await message.guild.fetch_member(int(bday_id))
+        bday_name = member.name
+        event_date =  event_arr[2][1:]
+        event_dt_obj = datetime.strptime(event_date, '%d/%m/%Y')
+        add_bday(int(bday_id), event_date, "0")
+        await message.channel.send("Bday of " + at_user(bday_id) +" has been added.")
+
+    #View all events command
+    if msg.startswith('$All'):
+      out = show_all()
+      await message.channel.send(out)
+        
+    #Delete event command
+    if msg.startswith('$DeleteEvent'):
+      del_arr = msg.split(", ")
+      event_name = del_arr[1]
+      delete_event(event_name)
+      await message.channel.send("Event " + event_name + " has been removed")
     
-    # 0 - command
-    # 1 - event name 
-    # 2 - event date
-
-    #get event parameters
-    event_name = event_arr[1]
-    event_date =  event_arr[2][1:]
-    event_remider =  event_arr[3][1:]
-    event_date_obj = datetime.strptime(event_date, '%d/%m/%Y %H:%M:%S')
-    event_remider_obj = datetime.strptime(event_remider, '%d/%m/%Y %H:%M:%S')
-    event_id = get_event_id()
+    #Delete Bday command
+    if msg.startswith('$DeleteBday'):
+        event_arr = msg.split(",");
+        bday_id = event_arr[1][4:-1]
+        member = client.get_user(int(bday_id))
+        bday_name = member.name
+        delete_bday(int(bday_id))
+        await message.channel.send("Birthday of " + bday_name +" has been removed.")
 
 
-    #add to DB
-    add_event(event_id, event_name, event_date, event_remider)
+    #Delete all events command
+    if msg.startswith('$DeleteAll'):
+      out = delete_all()
+      await message.channel.send(out)
 
-    #notification
-    print("Your event named: " + event_name + " that will occur on " + event_date + " has been added.")
-
-    await message.channel.send("Your event named: " + event_name + " that will occur on " + event_date + " has been added. Event ID: " + event_id)
-
-#Set Bday
-  if msg.startswith('$Bday'):
-    
-    event_arr = msg.split(",");
-    
-    # 0 - command
-    # 1 - bday user id 
-    # 2 - bday date
-
-    bday_id = event_arr[1][4:-1]
-    print("bday_id: ", bday_id)
-    member = await message.guild.fetch_member(int(bday_id))
-    bday_name = member.name
-    event_date =  event_arr[2][1:]
-    event_dt_obj = datetime.strptime(event_date, '%d/%m/%Y')
-
-    print("bday_name: ", bday_name)
-    print("Member: ", member)
-
-    await message.channel.send("Bday of " + at_user(bday_id) +" has been added.")
-
-#Delete evet
-  if msg.startswith('$Delete'):
-
-    del_arr = msg.split(", ")
-    del_id = int(del_arr[1])
-    deleted_event_name = delete_event(del_id)[0]
-    await message.channel.send("Event : " + deleted_event_name + " has been removed")
-
-
-
-#Objects
-
-
+    #Show todays events
+    if msg.startswith('$Today'):
+      out = today()
+      await message.channel.send(out)
 
 #Token
 client.run(os.getenv('TOKEN'))
-
-
-
-
